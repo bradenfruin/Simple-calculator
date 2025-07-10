d38978c50779e7e327968bf9c674fc910dea96df
Braden Fruin <btfruin57@gmail.com>
2025-07-10 17:46:19 -0400

    Update calculator.py
    Spelling errors

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Enter First Number: "))
 num_2 = int(input("Enter Second Number: "))
-print("Choose calculation to perform: \n","1. Add\n","2. Subtract\n","3. Multiply\n","4. Devide\n")
+print("Choose calculation to perform: \n","1. Add\n","2. Subtract\n","3. Multiply\n","4. Divide\n")
 cal = input().strip().lower()
 if cal == "add":
     ans = num_1 + num_2
@@ -8,9 +8,9 @@ elif cal == "subtract":
     ans = num_1 - num_2
 elif cal == "multiply":
     ans = num_1 * num_2
-elif cal == "devide":
+elif cal == "divide":
     if num_2 == 0:
-        print("Error!!!! Can't Devide by 0")
+        print("Error!!!! Can't Divide by 0")
         ans = None
     else:
         ans = num_1 / num_2
